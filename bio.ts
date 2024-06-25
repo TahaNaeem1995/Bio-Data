a9bc3d2428883285c46bcbe795792a99ef848955
TahaNaeem1995 <160178535+TahaNaeem1995@users.noreply.github.com>
2024-06-25 15:47:10 -0700

    Add files via upload

---
+++ bio.ts
@@ -0,0 +1,33 @@
+
+let address: string = "39/14 Tawakal view point (FSC) Nazimabad Block#1 "
+let city: string = "karachi"
+let country: string = "Pakitan" 
+
+// Education
+const matriculation: string ="Iqra Academy Jannat ul Atfal"
+const intermediate: string = "NJV Higher secondary School/collage Saddar Karachi"
+const Bachelors: string = "Dadabhoy Institute of Higher Education"
+
+// Skills
+const ComputerCourse: string = "CIT From BBC Institute Karachi"
+const Language: string = "English From Access microscholarship program USAID"
+const Technician: string = "Mobile Hardware"
+
+//printing Biodata
+console.log("Name:",fullName)
+console.log("Father Name:", fatherName)
+console.log("Age:", age)
+console.log("Email:",email)
+console.log("Contact:", contact)
+console.log("Religion:", religion)
+console.log("Married:", married)
+console.log("Address:",address)
+console.log("City:",city)
+console.log("Country:",country)
+console.log("Martriculation:",matriculation)
+console.log("Intermediate:",intermediate)
+console.log("Bachelors:",Bachelors)
+console.log("Computercourse:",ComputerCourse)
+console.log("Language:",Language)
+console.log("Technician:",Technician)
+
---
+++ biodata.js
@@ -0,0 +1,38 @@
+// Biodata using let,const,comments,string,number,boolean etc...
+// Personal information
+var fullName = "Taha Naeem";
+var fatherName = "Naeem noor Muhammad";
+var age = 28;
+var email = "tahanaeem513@gmail.com";
+var contact = "03132245327";
+var religion = "Islam";
+var married = true;
+// Address
+var address = "39/14 Tawakal view point (FSC) Nazimabad Block#1 ";
+var city = "karachi";
+var country = "Pakitan";
+// Education
+var matriculation = "Iqra Academy Jannat ul Atfal";
+var intermediate = "NJV Higher secondary School/collage Saddar Karachi";
+var Bachelors = "Dadabhoy Institute of Higher Education";
+// Skills
+var ComputerCourse = "CIT From BBC Institute Karachi";
+var Language = "English From Access microscholarship program USAID";
+var Technician = "Mobile Hardware";
+//printing Biodata
+console.log("Name:", fullName);
+console.log("Father Name:", fatherName);
+console.log("Age:", age);
+console.log("Email:", email);
+console.log("Contact:", contact);
+console.log("Religion:", religion);
+console.log("Married:", married);
+console.log("Address:", address);
+console.log("City:", city);
+console.log("Country:", country);
+console.log("Martriculation:", matriculation);
+console.log("Intermediate:", intermediate);
+console.log("Bachelors:", Bachelors);
+console.log("Computercourse:", ComputerCourse);
+console.log("Language:", Language);
+console.log("Technician:", Technician);
